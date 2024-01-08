2e5b71d71b57e511364a9330305d3d9acb1e8ddd
Tristan Weis <mail@tristanweis.com>
2024-01-09 00:03:01 +0100

    💄 add box shadow to nav bar

--- static/js/hero.js
+++ static/js/hero.js
@@ -2,6 +2,7 @@ function navBackgroundColor() {
   const nav = document.querySelector('#nav')
   const alpha = (window.scrollY || window.pageYOffset) / 128
   nav.style.backgroundColor = `rgb(34, 34, 34, ${alpha})`
+  nav.style.boxShadow = `0 0 1rem rgb(17, 17, 17, ${alpha})`
 }
 window.addEventListener('DOMContentLoaded', navBackgroundColor)
 window.addEventListener('scroll', navBackgroundColor)
